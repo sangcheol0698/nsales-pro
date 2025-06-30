e546c1c8ccedaee9e28615f6ca58644c2a5d2601
sangcheol <tkdcjf38@iabacus.co.kr>
2025-07-01 03:25:07 +0900

    refactor: DataTable 스켈레톤 로딩 제거 및 로딩 오버레이만 사용
    - DataTable 컴포넌트에서 스켈레톤 로딩 관련 코드 (Skeleton 컴포넌트 임포트 및 스켈레톤 행 렌더링 로직) 제거
    - 로딩 중에는 테이블 전체를 덮는 반투명 오버레이만 표시되도록 수정
    - TableEmpty 컴포넌트의 렌더링 조건을 로딩 상태와 무관하게 데이터 유무에만 따르도록 변경

--- src/core/components/ui/data-table/DataTable.vue
+++ src/core/components/ui/data-table/DataTable.vue
@@ -100,7 +100,7 @@ defineExpose({ table });
             </TableRow>
           </template>
         </template>
-        <template v-else-if="!loading">
+        <template v-else>
           <TableEmpty :colspan="columns.length">
             <div class="flex flex-col items-center">
               <p class="text-lg font-medium">{{ emptyMessage || '데이터가 없습니다' }}</p>
@@ -113,11 +113,6 @@ defineExpose({ table });
       </TableBody>
     </Table>
 
-    <div v-if="loading" class="absolute inset-0 bg-background/80 flex items-center justify-center z-10">
-      <svg class="animate-spin h-8 w-8 text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
-    </div>
+    <div v-if="loading" class="absolute inset-0 bg-background/80 flex items-center justify-center z-10"></div>
   </div>
 </template>
